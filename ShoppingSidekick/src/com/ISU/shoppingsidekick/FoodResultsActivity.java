7b14052df0e73c4a29d64c3feeb1539a9f94e20e
lrohret <lucasrohret@gmail.com>
2013-11-08 20:09:54 -0600

    improve Scan

--- ShoppingSidekick/src/com/ISU/shoppingsidekick/FoodResultsActivity.java
+++ ShoppingSidekick/src/com/ISU/shoppingsidekick/FoodResultsActivity.java
@@ -1,11 +1,16 @@
 package com.ISU.shoppingsidekick;
 
-import android.os.Bundle;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.ISU.shoppingsidekick.DatabaseAPI.Food;
 
 public class FoodResultsActivity extends Activity {
 
@@ -13,6 +18,34 @@ public class FoodResultsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_food_results);
+		
+		
+		String scanValue = getIntent().getExtras().getString("scanValue");
+		Food scannedFood = new DatabaseAPI().getFoodItemByID(scanValue);
+		String name = scannedFood.Name;
+		String brand = scannedFood.Brand;
+		String id = scannedFood.ID;
+		DatabaseAPI.Expiration expirationInfo = scannedFood.ExpirationInformation;
+		DatabaseAPI.Price priceInfo = scannedFood.PriceInformation;
+		List<DatabaseAPI.Review> reviewInfo = scannedFood.ReviewInformation;
+		
+		TextView productName = (TextView) findViewById(R.id.productName);
+		productName.setText(name);
+		
+		TextView productBrand = (TextView) findViewById(R.id.productBrand);
+		productBrand.setText(brand);
+		
+		TextView productID = (TextView) findViewById(R.id.productID);
+		productBrand.setText(id);
+		
+		TextView expInformation = (TextView) findViewById(R.id.expInformation);
+		productBrand.setText(expirationInfo.avgHours);
+		
+		TextView priceInformation = (TextView) findViewById(R.id.priceInformation);
+		productBrand.setText(priceInfo);
+		
+		TextView reviewInformation = (TextView) findViewById(R.id.reviewInformation);
+		productBrand.setText(reviewInfo);
 		
 		//confirmation button     
         Button goToScanBtn = (Button) findViewById(R.id.resultsToConfirmation);
